a3d5ee11e9d659ea441774a9e0f31d5b8472c970
Maris-Angelique <34096881+marisangelique@users.noreply.github.com>
2023-09-14 11:39:00 -1000

    Add files via upload
    For loop and converting letters into ASCII or unicode encoding systems

--- InClass.java
+++ InClass.java
@@ -24,6 +24,27 @@ public class InClass {
         for(int counter=0;counter<name.length();counter++){
             System.out.println(name.charAt(counter));
         }
+        
+        //Getting the first character of the name string
+        char letter = name.charAt(0);
+        //Creating a int variable
+        int shift = 3;
+        
+        //Output letter - letter is a character
+        System.out.println(letter);
+        
+        //Output letter as an integer (from ASCII or unicode encoding systems)
+        System.out.println((int)letter);
+        
+        //Adding the shift (+3) to the numeric value of the letter - note the (int) casting of letter
+        int newLetter = (int) letter + shift;
+        System.out.println(newLetter);
+        
+        //Output the new letter as a character - note the char casting of newLetter
+        System.out.println((char)newLetter);
+        
+        
+        
     }
     
 }
